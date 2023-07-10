61c0d07fbef754ff77e84d8b9e33d16d694e90f4
Denys Holius <denaspireone@gmail.com>
2023-07-10 15:52:35 +0300

    fixed generating VM dashboards for VM datasource

--- deployment/docker/add-vm-dashboards.sh
+++ deployment/docker/add-vm-dashboards.sh
@@ -11,4 +11,14 @@ for file in /tmp/dashboards/*.json; do sed -i 's/\"id\"\:\ \"prometheus\"/\"id\"
 for file in /tmp/dashboards/*.json; do sed -i  's/\"type\"\:\ \"prometheus\"/\"type\"\:\ \"victoriametrics-datasource\"/g' "$file"; done
 for file in /tmp/dashboards/*.json; do sed -i  's/\"name\"\:\ \"Prometheus\"/\"name\"\:\ \"VictoriaMetrics\"/g' "$file"; done
 
+for file in /tmp/dashboards/*.json; do sed -i  's/\"query\"\:\ \"prometheus\"/\"query\"\:\ \"victoriametrics-datasource\"/g' "$file"; done
+
+for file in /tmp/dashboards/*.json; do sed -i  's/\"uid\"\:\ \"wNf0q_kZk\"/\"uid\"\:\ \"wNf0q_kZkVM\"/g' "$file"; done
+for file in /tmp/dashboards/*.json; do sed -i  's/\"uid\"\:\ \"G7Z9GzMGz\"/\"uid\"\:\ \"G7Z9GzMGzVM\"/g' "$file"; done
+for file in /tmp/dashboards/*.json; do sed -i  's/\"uid\"\:\ \"LzldHAVnz\"/\"uid\"\:\ \"LzldHAVnzVM\"/g' "$file"; done
+
+for file in /tmp/dashboards/*.json; do sed -i  's/\"title\"\:\ \"VictoriaMetrics\"/\"title\"\:\ \"VictoriaMetrics - VM DS\"/g' "$file"; done
+for file in /tmp/dashboards/*.json; do sed -i  's/\"title\"\:\ \"VictoriaMetrics\ -\ vmagent\"/\"title\"\:\ \"VictoriaMetrics\ -\ vmagent\ VM \DS\"/g' "$file"; done
+for file in /tmp/dashboards/*.json; do sed -i  's/\"title\"\:\ \"VictoriaMetrics\ -\ vmalert\"/\"title\"\:\ \"VictoriaMetrics\ -\ vmalert\ VM \DS\"/g' "$file"; done
+
 cp -r /tmp/dashboards/*.json ./dashboards/
