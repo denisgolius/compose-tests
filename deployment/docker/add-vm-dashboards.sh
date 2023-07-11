ae7431af8ea3107d90ae2510f145fc0711b9c16f
Denys Holius <denaspireone@gmail.com>
2023-07-11 21:06:57 +0300

    wip

--- deployment/docker/add-vm-dashboards.sh
+++ deployment/docker/add-vm-dashboards.sh
@@ -3,9 +3,13 @@ set -ex
 
 mkdir -p /tmp/dashboards
 
-cp ./dashboards/victoriametrics.json /tmp/dashboards/victoriametrics-vm-ds.json
-cp ./dashboards/vmagent.json /tmp/dashboards/vmagent-vm-ds.json
-cp ./dashboards/vmalert.json /tmp/dashboards/vmalert-vm-ds.json
+# cp ./dashboards/victoriametrics.json /tmp/dashboards/victoriametrics-vm-ds.json
+# cp ./dashboards/vmagent.json /tmp/dashboards/vmagent-vm-ds.json
+# cp ./dashboards/vmalert.json /tmp/dashboards/vmalert-vm-ds.json
+
+cp /var/lib/grafana/dashboards/vm.json /tmp/dashboards/victoriametrics-vm-ds.json
+cp /var/lib/grafana/dashboards/vmagent.json /tmp/dashboards/vmagent-vm-ds.json
+cp /var/lib/grafana/dashboards/vmalert.json /tmp/dashboards/vmalert-vm-ds.json
 
 for file in /tmp/dashboards/*.json; do sed -i 's/\"id\"\:\ \"prometheus\"/\"id\"\:\ \"victoriametrics-datasource\"/g' "$file"; done
 for file in /tmp/dashboards/*.json; do sed -i  's/\"type\"\:\ \"prometheus\"/\"type\"\:\ \"victoriametrics-datasource\"/g' "$file"; done
@@ -21,4 +25,5 @@ for file in /tmp/dashboards/*.json; do sed -i  's/\"title\"\:\ \"VictoriaMetrics
 for file in /tmp/dashboards/*.json; do sed -i  's/\"title\"\:\ \"VictoriaMetrics\ -\ vmagent\"/\"title\"\:\ \"VictoriaMetrics\ -\ vmagent\ VM \DS\"/g' "$file"; done
 for file in /tmp/dashboards/*.json; do sed -i  's/\"title\"\:\ \"VictoriaMetrics\ -\ vmalert\"/\"title\"\:\ \"VictoriaMetrics\ -\ vmalert\ VM \DS\"/g' "$file"; done
 
-cp -r /tmp/dashboards/*.json ./dashboards/
+# cp -r /tmp/dashboards/*.json ./dashboards/
+cp -r /tmp/dashboards/*.json /var/lib/grafana/dashboards
